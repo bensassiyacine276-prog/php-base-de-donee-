fee232fba93ead4fc55ea02fd9f814c0700733fe
bensassiyacine276-prog <bensassiyacine276@gmail.com>
2026-02-15 17:37:44 +0100

    Delete credentials.example.php

--- credentials.example.php
+++
@@ -1,9 +0,0 @@
-<?php
-// Configuration base de données
-// Copier ce fichier en credentials.php et modifier les valeurs
-
-define('DB_HOST', 'localhost');
-define('DB_NAME', 'ma_base');
-define('DB_USER', 'root');
-define('DB_PASS', '');
-?>
